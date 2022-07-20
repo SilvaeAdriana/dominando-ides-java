95ce5c032939e5dfd68d5f76f38a806ec36d02f7
A Driana <75712203+SilvaeAdriana@users.noreply.github.com>
2022-07-19 22:39:44 -0300

    Main e class java.

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		
+		Gato gato = new Gato(null, null, null);
+		
+		System.out.print(gato);
+		
+
+	}
+
+}
